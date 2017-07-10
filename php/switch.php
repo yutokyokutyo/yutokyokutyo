a09c6f221287e0c42f381fa1939e2c6bdc7a93d3
y.kondo <k.yuto.cre@gmail.com>
2017-07-10 17:22:10 +0900

    switch

---
+++ php/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$i = 3;
+
+switch ($i) {
+case 0:
+    echo "0 dayo!";
+    break;
+case 1:
+    echo "1 dayo";
+    break;
+case 2:
+    echo "2 dayo";
+    break;
+default:
+    echo "我！天涯孤独！";
+}
