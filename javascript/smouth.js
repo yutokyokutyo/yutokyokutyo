49be36387e0a2f895d039ef704a5d1b563d1d874
y.kondo <k.yuto.cre@gmail.com>
2017-10-21 23:34:28 +0900

    smooth case

---
+++ javascript/smouth.js
@@ -0,0 +1,17 @@
+jQuery.easing.quart = function (x, t, b, c, d) {
+    return -c * ((t=t/d-1)*t*t*t - 1) + b;
+};
+ 
+$(function(){
+    jQuery('.scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var $target = jQuery(this.hash);
+            $target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+            var targetOffset = $target.offset().top;
+            jQuery('html,body').animate({ scrollTop: targetOffset }, 1200, 'quart');
+            return false;
+            }
+        }
+    });
+});
