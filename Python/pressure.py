67016ae5e5471fa1b2f44cbe576d1b9946f015d9
y.kondo <k.yuto.cre@gmail.com>
2018-02-02 01:45:51 +0900

    入浴したら音声を出力する

--- Python/pressure.py
+++ Python/pressure.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import sys
 import spidev
 import time
 import os
+import pygame.mixer
 
 # SPIバスを開く
 spi = spidev.SpiDev()
@@ -13,33 +13,43 @@ spi.open(0,0)
 # MCP3008から値を読み取るメソッド
 # チャンネル番号は0から7まで
 def ReadChannel(channel):
- adc = spi.xfer2([1,(8+channel)<<4,0])
- data = ((adc[1]&3) << 8) + adc[2]
- return data
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
 
 # 得た値を電圧に変換するメソッド
 # 指定した桁数で切り捨てる
 def ConvertVolts(data,places):
- volts = (data * 5) / float(1023)
- volts = round(volts,places)
- return volts
+  volts = (data * 5) / float(1023)
+  volts = round(volts,places)
+  return volts
 
 # センサのチャンネルの宣言
 force_channel = 0
-
 # 値を読むのを遅らせる
 delay = 0.25
 
 # メインクラス
 if __name__ == '__main__':
- try:
-  while True:
-   data = ReadChannel(force_channel)
-   print("A/D Converter: {0}".format(data))
-   volts = ConvertVolts(data,3)
-   print("Volts: {0}".format(volts))
-   time.sleep(delay)
-# 何か入力したら終了
- except KeyboardInterrupt:
-  spi.close()
-  sys.exit(0)
+  try:
+    while True:
+      data = ReadChannel(force_channel)
+      print("A/D Converter: {0}".format(data))
+      volts = ConvertVolts(data,3)
+      print("Volts: {0}".format(volts))
+
+      # A/D値が500を下回ったら入浴したと見なす
+      if(data > 0 and data < 500):
+        print("入浴なう！")
+        # 音楽再生
+        pygame.mixer.init()
+        pygame.mixer.music.load("snowboy.wav")
+        pygame.mixer.music.play(1)
+      else:
+        print("退出！！")
+
+      time.sleep(delay)
+ # 何か入力したら終了
+  except KeyboardInterrupt:
+   spi.close()
+   sys.exit(0)
